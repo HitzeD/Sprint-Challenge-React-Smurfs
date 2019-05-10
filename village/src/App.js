6c2de7db942a7e4f070452ae845ef349493dcb3e
HitzeD <d-hitze@comcast.net>
2019-05-10 10:38:04 -0500

    Routes added move onto navbar

--- village/src/App.js
+++ village/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -30,8 +31,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addNew={this.addSmurf}/>
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route path="/smurfs-form" render={props => (
+          <SmurfForm {...props} addNew={this.addSmurf}/>
+        )} />
+        <Route exact path="/" render={props => (
+          <Smurfs {...props} smurfs={this.state.smurfs} />
+        )} />
       </div>
     );
   }
